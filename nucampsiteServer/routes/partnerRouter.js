980899e461f7577580ed7d84b01fdae62df79109
Tori <torihoward27@yahoo.com>
2023-11-15 13:12:39 -0500

    CORS

--- nucampsiteServer/routes/partnerRouter.js
+++ nucampsiteServer/routes/partnerRouter.js
@@ -2,14 +2,16 @@ const express = require('express');
 const partnerRouter = express.Router();
 const Partner = require('../models/partner');
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 partnerRouter.route('/')
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
 .all((req,res,next) =>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
-.get((req,res, next) => {
+.get(cors.cors, (req,res, next) => {
     Partner.find()
     .then(partners => {
         res.statusCode = 200;
@@ -18,7 +20,7 @@ partnerRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next)=> {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next)=> {
     Partner.create(req.body)
     .then(partner => {
         console.log('Partner Created', partner);
@@ -28,11 +30,11 @@ partnerRouter.route('/')
     })
     .catch(err => next(err));
 })
-.put(authenticate.verifyUser, (req,res) =>{
+.put(cors.corsWithOptions, authenticate.verifyUser, (req,res) =>{
     res.statusCode = 403;
     res.end('PUT operation not supported on /partners');
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next) => {
     Partner.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -43,7 +45,8 @@ partnerRouter.route('/')
 })
 
 partnerRouter.route('/:partnerId')
-.get((req,res,next) =>{
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req,res,next) =>{
     Partner.findbyId(req.params.partnerId)
     .then(partner => {
         res.statusCode = 200;
@@ -52,11 +55,11 @@ partnerRouter.route('/:partnerId')
     })
     .catch(err => next(err));
 })
-.post(authenticate.verifyUser, (req,res) =>{
+.post(cors.corsWithOptions, authenticate.verifyUser, (req,res) =>{
     res.statusCode = 403;
     res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next) =>{
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next) =>{
     Partner.findByIdAndUpdate(req.params.partnerId, {
         $set: req.body
     }, {new: true})
@@ -67,7 +70,7 @@ partnerRouter.route('/:partnerId')
     })
     .catch(err => next(err));
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req,res, next) => {
     Partner.findByIdAndDelete(req.params.partnerId)
     .then(response => {
         res.statusCode = 200;
--- nucampsiteServer/routes/users.js
+++ nucampsiteServer/routes/users.js
@@ -2,11 +2,13 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
+const cors = require('./cors');
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+router.options(cors.corsWithOptions, (req, res) => res.sendStatus(200));
+router.get('/',cors.cors, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     User.find()
     .then((users) => {
         res.statusCode = 200;
@@ -16,7 +18,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, ne
     .catch(err => next(err));
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', cors.corsWithOptions, (req, res) => {
     User.register(
         new User({username: req.body.username}),
         req.body.password,
@@ -50,7 +52,7 @@ router.post('/signup', (req, res) => {
     );
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
     const token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -58,7 +60,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
 });
 
 
-router.get('/logout', (req, res, next) => {
+router.get('/logout', cors.cors, (req, res, next) => {
     if (req.session) {
         req.session.destroy();
         res.clearCookie('session-id');
